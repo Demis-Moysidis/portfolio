632b87db317711d30c5edefe5a1d220a7250644a
Demis Moysidis <demis.moysidis@outlook.de>
2025-08-27 21:32:58 +0200

    feat: hero section - implementation of ngx translate

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,17 +1,21 @@
 import { NgClass } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 
 
 @Component({
   selector: 'app-header',
-  imports: [NgClass],
+  imports: [NgClass, TranslatePipe],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
 export class Header {
   currentLanguage = 'en';
 
+  private translate = inject(TranslateService);
+
   toggleLanguage(language: string) {
       this.currentLanguage = language;
+      this.translate.use(language);
   }
 }
