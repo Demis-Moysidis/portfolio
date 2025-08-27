42dbb99124ccae2e3db0d4b42a38c36adadbb1eb
Demis Moysidis <demis.moysidis@outlook.de>
2025-08-27 18:30:37 +0200

    feat: hero section - animation for language toggle uand hover effect h1

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,11 +1,17 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
 export class Header {
+  currentLanguage = 'en';
 
+  toggleLanguage(language: string) {
+      this.currentLanguage = language;
+  }
 }
