c88b64a2ac4523fc86f6ebf6ab05f2527fe5d9eb
Demis Moysidis <demis.moysidis@outlook.de>
2025-08-31 02:05:40 +0200

    feat: projects section

--- src/app/main-page/main-page.ts
+++ src/app/main-page/main-page.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Hero } from './hero/hero';
 import { AboutMe } from './about-me/about-me';
 import { Skills } from "./skills/skills";
+import { Projects } from "./projects/projects";
 
 @Component({
   selector: 'app-main-page',
-  imports: [Hero, AboutMe, Skills],
+  imports: [Hero, AboutMe, Skills, Projects],
   templateUrl: './main-page.html',
   styleUrl: './main-page.scss'
 })
