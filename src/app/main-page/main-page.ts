da487a8d2536790da07b55b61a8c2152a38be2b1
Demis Moysidis <demis.moysidis@outlook.de>
2025-09-02 14:16:21 +0200

    feat: colleagues' thoughts section

--- src/app/main-page/main-page.ts
+++ src/app/main-page/main-page.ts
@@ -3,10 +3,11 @@ import { Hero } from './hero/hero';
 import { AboutMe } from './about-me/about-me';
 import { Skills } from "./skills/skills";
 import { Projects } from "./projects/projects";
+import { ColleaguesThoughts } from "./colleagues-thoughts/colleagues-thoughts";
 
 @Component({
   selector: 'app-main-page',
-  imports: [Hero, AboutMe, Skills, Projects],
+  imports: [Hero, AboutMe, Skills, Projects, ColleaguesThoughts],
   templateUrl: './main-page.html',
   styleUrl: './main-page.scss'
 })
