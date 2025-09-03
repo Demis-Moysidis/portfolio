a0fdf257e229f1960774b3d8e29bad4af3d36f50
Demis Moysidis <demis.moysidis@outlook.de>
2025-09-03 11:56:36 +0200

    feat: colleagues' thoughts translation into german

--- src/app/main-page/colleagues-thoughts/colleagues-thoughts.ts
+++ src/app/main-page/colleagues-thoughts/colleagues-thoughts.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-colleagues-thoughts',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './colleagues-thoughts.html',
   styleUrl: './colleagues-thoughts.scss'
 })
